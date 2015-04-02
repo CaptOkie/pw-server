504260301e4d0899e2672843a766f4f86b26f1a2
Juhandre <juhandreknoetze@cmail.carleton.ca>
2015-04-01 22:43:13 -0400

    Specified actual database file

--- dbs/user-db.js
+++ dbs/user-db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3');
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database('./dbs/pw_db');
 
 // ========================================================================= //
 // DB CONSTANTS                                                              //
